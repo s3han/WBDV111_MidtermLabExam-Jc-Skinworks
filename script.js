96bf0128caa17bf3f2ae059603994a044ad8e8e4
s3han <rjostan2849ant@student.fatima.edu.ph>
2026-05-04 15:30:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ menuOpenButton.addEventListener("click", () => {
 });
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+new Swiper('.card-wrapper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 25,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
